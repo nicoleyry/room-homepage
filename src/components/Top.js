e73e1fd5e36b15501bcb55335ec2ef7aa4b3de60
Nicole Yang <nicoleyry@outlook.com>
2023-09-13 16:30:01 +0800

    add top slides

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,18 +1,28 @@
 import '../styles/top.scss';
 import ControllerLeft from '../assets/images/icon-angle-left.svg';
 import ControllerRight from '../assets/images/icon-angle-right.svg';
+import { useState } from 'react';
+import { slidesData } from '../slidesData';
 
 export default function Top() {
+	const [slide, setSlide] = useState(1);
+
+	let prev = () => {
+		slide === 1 ? setSlide(slidesData.length) : setSlide(n => n - 1);
+	};
+
+	let next = () => {
+		slide === slidesData.length ? setSlide(1) : setSlide(n => n + 1);
+	};
+	
 	return (
 		<div className='Top'>
-			<div className='img-container'></div>
+			<div className={`img-container slide-${slide}`} style={{backgroundImage: `url('${slidesData[slide-1]['image']}')`}}></div>
 			<div className='content-container'>
-				<h1 className='title'>Discover innovative ways to decorate</h1>
-				<p className='content'>
-					We provide unmatched quality, comfort, and style for property owners across the country. Our experts
-					combine form and function in bringing your vision to life. Create a room in your own style with our
-					collection and make your property a reflection of you and what you love.
-				</p>
+				<div className="content-slide">
+					<h1 className='title'>{slidesData[slide-1]['title']}</h1>
+					<p className='content'>{slidesData[slide-1]['content']}</p>
+				</div>
 				<div className='shop-now'>
 					<p className='shop-now-text'>Shop Now</p>
 					<svg className='shop-now-arrow' width='40' height='12' xmlns='http://www.w3.org/2000/svg'>
@@ -25,10 +35,10 @@ export default function Top() {
 				</div>
 
 				<div className='controllers-container'>
-					<div className='controller controller-left'>
+					<div className='controller controller-left' onClick={prev}>
 						<img src={ControllerLeft} alt='left' />
 					</div>
-					<div className='controller controller-right'>
+					<div className='controller controller-right' onClick={next}>
 						<img src={ControllerRight} alt='right' />
 					</div>
 				</div>
