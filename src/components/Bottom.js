f8bab37b80e644f9b1d5767553bbc49dce035389
Nicole Yang <nicoleyry@outlook.com>
2023-09-14 14:14:21 +0800

    add mobile bottom section

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -3,7 +3,7 @@ import '../styles/bottom.scss';
 export default function Bottom() {
 	return (
 		<div className="Bottom">
-			<div className="img-container img-container-dark">\</div>
+			<div className="img-container img-container-dark"></div>
 			<div className="about">
 				<p className="title">About Our Furniture</p>
 				<p className="content">Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.</p>
